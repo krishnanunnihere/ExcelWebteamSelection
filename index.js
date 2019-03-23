3263933748044759dcfda25b45f353a7fe566158
krishnanunnihere <krishnanunnitk1999@gmail.com>
2019-03-23 21:03:34 +0530

    changes made

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-function printData(){
-    let title = document.getElementById('username').value;
-    let body = document.getElementById('email').value;
-  
-      fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST', 
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    },
-    body: JSON.stringify({title:title, body:body})
-    })
-    .then(response => response.json())
-    .then(data => {
-      window.alert(JSON.stringify(data));
-      console.log(data);
-    })
-    .catch(error => console.error('Error:', error));
-  }
+
+function printtData(){
+  let title = document.getElementById('username').value;
+  let body = document.getElementById('comment').value;
+
+	fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST', 
+  headers: {
+    "Content-type": "application/json; charset=UTF-8"
+  },
+  body: JSON.stringify({title:title, body:body})
+  })
+  .then(response => response.json())
+  .then(data => {
+    window.alert(JSON.stringify(data));
+    console.log(data);
+  })
+  .catch(error => console.error('Error:', error));
+}
